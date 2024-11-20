37a5c8106e1e279d883df4300bcc77f6a4852b4c
zev-b <zevbistritz@gmail.com>
2024-11-20 18:32:37 -0500

    first version of model and migration for tag

---
+++ backend/db/migrations/20241120232010-create-tag.js
@@ -0,0 +1,35 @@
+'use strict';
+
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;  // define your schema in options object
+}
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Tags', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    }, options);
+  },
+  async down(queryInterface, Sequelize) {
+    options.tableName = 'Tags';
+    await queryInterface.dropTable(options);
+  }
+};
---
+++ backend/db/models/tag.js
@@ -0,0 +1,28 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Tag extends Model {
+ 
+    static associate(models) {
+      // Many-to-Many relationship with ArtPiece via ArtTag
+      Tag.belongsToMany(models.ArtPiece, {
+        through: models.ArtTag,
+        foreignKey: 'tagId',
+        otherKey: 'artId',
+      });
+    }
+  }
+  Tag.init({
+    name: {
+      type: DataTypes.STRING(24),
+      allowNull: false,
+      unique: true,
+    },
+  }, {
+    sequelize,
+    modelName: 'Tag',
+  });
+  return Tag;
+};
