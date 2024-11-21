96c380f0e84c8f7b070f01bcbfc130f10c7971b8
zev-b <zevbistritz@gmail.com>
2024-11-21 12:49:31 -0500

    adds tags seeder data

---
+++ backend/db/seeders/20241121173611-create-tags.js
@@ -0,0 +1,53 @@
+'use strict';
+
+const { Tag } = require('../models');
+
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;  // define your schema in options object
+}
+
+//^ --------------- Seeder Data ---------------
+const seedData = [
+  {
+    name: 'Jerusalem-Urban'
+  },
+  {
+    name: 'Portrait'
+  },
+  {
+    name: 'Landscape'
+  },
+  {
+    name: 'Concept'
+  },
+  {
+    name: 'Still-Life'
+  },
+  {
+    name: 'Sketch'
+  },
+  {
+    name: 'Tradition'
+  },
+  {
+    name: 'Watercolor'
+  },
+  {
+    name: 'Inspiration'
+  },
+]
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await Tag.bulkCreate(seedData, { validate: true });
+  },
+
+  async down (queryInterface, Sequelize) {
+    options.tableName = 'Tags';
+    for (const tag of seedData) {
+      await Tag.destroy({ where: tag });
+    } 
+  }
+};
