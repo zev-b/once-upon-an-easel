0d87fba6a94d3aa9e5156fc5411facee524bfbd9
zev-b <zevbistritz@gmail.com>
2024-11-21 14:04:04 -0500

    adds cyrillic placeholder text to artpiece descriptions

--- backend/db/seeders/20241121151350-create-artpieces.js
+++ backend/db/seeders/20241121151350-create-artpieces.js
@@ -13,210 +13,210 @@ const seedData = [
     userId: 1,
     imageId: 'https://once-upon-an-easel-bucket.s3.us-east-1.amazonaws.com/usher-art/candles.jpg',
     title: 'Candles',
-    description: '',
+    description: 'Текст-рыба для веб-дизайна: лорем ипсум долор сит амет, consectetur adipiscing элит. Элиас интеллегам мел ид, при пертинах хонистас ут. Не про вирис сапиентем, вис номинави детрахит адиписцинг ут. Яуидам сенсибус вис ид, ин про убикве ирьяредем. Эос ут обликве веритус.',
     available: true
   },
   {
     userId: 1,
     imageId: 'https://once-upon-an-easel-bucket.s3.us-east-1.amazonaws.com/usher-art/portrait_1.jpg',
     title: 'Portrait',
-    description: '',
+    description: 'Текст-рыба для веб-дизайна: лорем ипсум долор сит амет, consectetur adipiscing элит. Элиас интеллегам мел ид, при пертинах хонистас ут. Не про вирис сапиентем, вис номинави детрахит адиписцинг ут. Яуидам сенсибус вис ид, ин про убикве ирьяредем. Эос ут обликве веритус.',
     available: true
   },
   {
     userId: 1,
     imageId: 'https://once-upon-an-easel-bucket.s3.us-east-1.amazonaws.com/usher-art/colorincup.jpg',
     title: 'Color in Cup',
-    description: '',
+    description: 'Текст-рыба для веб-дизайна: лорем ипсум долор сит амет, consectetur adipiscing элит. Элиас интеллегам мел ид, при пертинах хонистас ут. Не про вирис сапиентем, вис номинави детрахит адиписцинг ут. Яуидам сенсибус вис ид, ин про убикве ирьяредем. Эос ут обликве веритус.',
     available: true
   },
   {
     userId: 2,
     imageId: 'https://once-upon-an-easel-bucket.s3.us-east-1.amazonaws.com/usher-art/portrait_2.jpg',
     title: 'Portrait 2',
-    description: '',
+    description: 'Текст-рыба для веб-дизайна: лорем ипсум долор сит амет, consectetur adipiscing элит. Элиас интеллегам мел ид, при пертинах хонистас ут. Не про вирис сапиентем, вис номинави детрахит адиписцинг ут. Яуидам сенсибус вис ид, ин про убикве ирьяредем. Эос ут обликве веритус.',
     available: true
   },
   {
     userId: 2,
     imageId: 'https://once-upon-an-easel-bucket.s3.us-east-1.amazonaws.com/usher-art/bababasra74a.jpg',
     title: 'Baba Basra folio 74a',
-    description: '',
+    description: 'Текст-рыба для веб-дизайна: лорем ипсум долор сит амет, consectetur adipiscing элит. Элиас интеллегам мел ид, при пертинах хонистас ут. Не про вирис сапиентем, вис номинави детрахит адиписцинг ут. Яуидам сенсибус вис ид, ин про убикве ирьяредем. Эос ут обликве веритус.',
     available: true
   },
   {
     userId: 3,
     imageId: 'https://once-upon-an-easel-bucket.s3.us-east-1.amazonaws.com/usher-art/portrait_3.jpg',
     title: 'Portrait 3',
-    description: '',
+    description: 'Текст-рыба для веб-дизайна: лорем ипсум долор сит амет, consectetur adipiscing элит. Элиас интеллегам мел ид, при пертинах хонистас ут. Не про вирис сапиентем, вис номинави детрахит адиписцинг ут. Яуидам сенсибус вис ид, ин про убикве ирьяредем. Эос ут обликве веритус.',
     available: true
   },
   {
     userId: 3,
     imageId: 'https://once-upon-an-easel-bucket.s3.us-east-1.amazonaws.com/usher-art/portrait_4.jpg',
     title: 'Portrait 4',
-    description: '',
+    description: 'Текст-рыба для веб-дизайна: лорем ипсум долор сит амет, consectetur adipiscing элит. Элиас интеллегам мел ид, при пертинах хонистас ут. Не про вирис сапиентем, вис номинави детрахит адиписцинг ут. Яуидам сенсибус вис ид, ин про убикве ирьяредем. Эос ут обликве веритус.',
     available: true
   },
   {
     userId: 3,
     imageId: 'https://once-upon-an-easel-bucket.s3.us-east-1.amazonaws.com/usher-art/yamhatalmud.jpg',
     title: 'Yam haTalmud',
-    description: '',
+    description: 'Текст-рыба для веб-дизайна: лорем ипсум долор сит амет, consectetur adipiscing элит. Элиас интеллегам мел ид, при пертинах хонистас ут. Не про вирис сапиентем, вис номинави детрахит адиписцинг ут. Яуидам сенсибус вис ид, ин про убикве ирьяредем. Эос ут обликве веритус.',
     available: true
   },
   {
     userId: 3,
     imageId: 'https://once-upon-an-easel-bucket.s3.us-east-1.amazonaws.com/usher-art/bimainshul.jpg',
     title: 'Bima in Shul',
-    description: '',
+    description: 'Текст-рыба для веб-дизайна: лорем ипсум долор сит амет, consectetur adipiscing элит. Элиас интеллегам мел ид, при пертинах хонистас ут. Не про вирис сапиентем, вис номинави детрахит адиписцинг ут. Яуидам сенсибус вис ид, ин про убикве ирьяредем. Эос ут обликве веритус.',
     available: true
   },
   {
     userId: 3,
     imageId: 'https://once-upon-an-easel-bucket.s3.us-east-1.amazonaws.com/usher-art/thehills.jpg',
     title: 'The Hills',
-    description: '',
+    description: 'Текст-рыба для веб-дизайна: лорем ипсум долор сит амет, consectetur adipiscing элит. Элиас интеллегам мел ид, при пертинах хонистас ут. Не про вирис сапиентем, вис номинави детрахит адиписцинг ут. Яуидам сенсибус вис ид, ин про убикве ирьяредем. Эос ут обликве веритус.',
     available: true
   },
   {
     userId: 4,
     imageId: 'https://once-upon-an-easel-bucket.s3.us-east-1.amazonaws.com/usher-art/lillypads.jpg',
     title: 'Lilly Pads',
-    description: '',
+    description: 'Текст-рыба для веб-дизайна: лорем ипсум долор сит амет, consectetur adipiscing элит. Элиас интеллегам мел ид, при пертинах хонистас ут. Не про вирис сапиентем, вис номинави детрахит адиписцинг ут. Яуидам сенсибус вис ид, ин про убикве ирьяредем. Эос ут обликве веритус.',
     available: true
   },
   {
     userId: 4,
     imageId: 'https://once-upon-an-easel-bucket.s3.us-east-1.amazonaws.com/usher-art/olivebranch.jpg',
     title: 'Olive Branch',
-    description: '',
+    description: 'Текст-рыба для веб-дизайна: лорем ипсум долор сит амет, consectetur adipiscing элит. Элиас интеллегам мел ид, при пертинах хонистас ут. Не про вирис сапиентем, вис номинави детрахит адиписцинг ут. Яуидам сенсибус вис ид, ин про убикве ирьяредем. Эос ут обликве веритус.',
     available: true
   },
   {
     userId: 4,
     imageId: 'https://once-upon-an-easel-bucket.s3.us-east-1.amazonaws.com/usher-art/field.jpg',
     title: 'Field',
-    description: '',
+    description: 'Текст-рыба для веб-дизайна: лорем ипсум долор сит амет, consectetur adipiscing элит. Элиас интеллегам мел ид, при пертинах хонистас ут. Не про вирис сапиентем, вис номинави детрахит адиписцинг ут. Яуидам сенсибус вис ид, ин про убикве ирьяредем. Эос ут обликве веритус.',
     available: true
   },
   {
     userId: 4,
     imageId: 'https://once-upon-an-easel-bucket.s3.us-east-1.amazonaws.com/usher-art/yearning.jpg',
     title: 'Yearning',
-    description: '',
+    description: 'Текст-рыба для веб-дизайна: лорем ипсум долор сит амет, consectetur adipiscing элит. Элиас интеллегам мел ид, при пертинах хонистас ут. Не про вирис сапиентем, вис номинави детрахит адиписцинг ут. Яуидам сенсибус вис ид, ин про убикве ирьяредем. Эос ут обликве веритус.',
     available: true
   },
   {
     userId: 5,
     imageId: 'https://once-upon-an-easel-bucket.s3.us-east-1.amazonaws.com/usher-art/tzedaka.jpg',
     title: 'Tzedaka',
-    description: '',
+    description: 'Текст-рыба для веб-дизайна: лорем ипсум долор сит амет, consectetur adipiscing элит. Элиас интеллегам мел ид, при пертинах хонистас ут. Не про вирис сапиентем, вис номинави детрахит адиписцинг ут. Яуидам сенсибус вис ид, ин про убикве ирьяредем. Эос ут обликве веритус.',
     available: true
   },
   {
     userId: 5,
     imageId: 'https://once-upon-an-easel-bucket.s3.us-east-1.amazonaws.com/usher-art/urban_1.jpg',
     title: 'Urban',
-    description: '',
+    description: 'Текст-рыба для веб-дизайна: лорем ипсум долор сит амет, consectetur adipiscing элит. Элиас интеллегам мел ид, при пертинах хонистас ут. Не про вирис сапиентем, вис номинави детрахит адиписцинг ут. Яуидам сенсибус вис ид, ин про убикве ирьяредем. Эос ут обликве веритус.',
     available: true
   },
   {
     userId: 5,
     imageId: 'https://once-upon-an-easel-bucket.s3.us-east-1.amazonaws.com/usher-art/urban_2.jpg',
     title: 'Urban 2',
-    description: '',
+    description: 'Текст-рыба для веб-дизайна: лорем ипсум долор сит амет, consectetur adipiscing элит. Элиас интеллегам мел ид, при пертинах хонистас ут. Не про вирис сапиентем, вис номинави детрахит адиписцинг ут. Яуидам сенсибус вис ид, ин про убикве ирьяредем. Эос ут обликве веритус.',
     available: true
   },
   {
     userId: 5,
     imageId: 'https://once-upon-an-easel-bucket.s3.us-east-1.amazonaws.com/usher-art/urban_3.jpg',
     title: 'Urban 3',
-    description: '',
+    description: 'Текст-рыба для веб-дизайна: лорем ипсум долор сит амет, consectetur adipiscing элит. Элиас интеллегам мел ид, при пертинах хонистас ут. Не про вирис сапиентем, вис номинави детрахит адиписцинг ут. Яуидам сенсибус вис ид, ин про убикве ирьяредем. Эос ут обликве веритус.',
     available: true
   },
   {
     userId: 6,
     imageId: 'https://once-upon-an-easel-bucket.s3.us-east-1.amazonaws.com/usher-art/yellowfieldsjpg',
     title: 'Yellow Fields',
-    description: '',
+    description: 'Текст-рыба для веб-дизайна: лорем ипсум долор сит амет, consectetur adipiscing элит. Элиас интеллегам мел ид, при пертинах хонистас ут. Не про вирис сапиентем, вис номинави детрахит адиписцинг ут. Яуидам сенсибус вис ид, ин про убикве ирьяредем. Эос ут обликве веритус.',
     available: true
   },
   {
     userId: 6,
     imageId: 'https://once-upon-an-easel-bucket.s3.us-east-1.amazonaws.com/usher-art/nachlaot.jpg',
     title: 'Nachlaot',
-    description: '',
+    description: 'Текст-рыба для веб-дизайна: лорем ипсум долор сит амет, consectetur adipiscing элит. Элиас интеллегам мел ид, при пертинах хонистас ут. Не про вирис сапиентем, вис номинави детрахит адиписцинг ут. Яуидам сенсибус вис ид, ин про убикве ирьяредем. Эос ут обликве веритус.',
     available: true
   },
   {
     userId: 6,
     imageId: 'https://once-upon-an-easel-bucket.s3.us-east-1.amazonaws.com/usher-art/chavrusah.jpg',
     title: 'Chavrusah',
-    description: '',
+    description: 'Текст-рыба для веб-дизайна: лорем ипсум долор сит амет, consectetur adipiscing элит. Элиас интеллегам мел ид, при пертинах хонистас ут. Не про вирис сапиентем, вис номинави детрахит адиписцинг ут. Яуидам сенсибус вис ид, ин про убикве ирьяредем. Эос ут обликве веритус.',
     available: true
   },
   {
     userId: 6,
     imageId: 'https://once-upon-an-easel-bucket.s3.us-east-1.amazonaws.com/usher-art/mosheinthesuf.jpg',
     title: 'Moshe in the Suf',
-    description: '',
+    description: 'Текст-рыба для веб-дизайна: лорем ипсум долор сит амет, consectetur adipiscing элит. Элиас интеллегам мел ид, при пертинах хонистас ут. Не про вирис сапиентем, вис номинави детрахит адиписцинг ут. Яуидам сенсибус вис ид, ин про убикве ирьяредем. Эос ут обликве веритус.',
     available: true
   },
   {
     userId: 6,
     imageId: 'https://once-upon-an-easel-bucket.s3.us-east-1.amazonaws.com/usher-art/fromthedepths.jpg',
     title: 'From the Depths',
-    description: '',
+    description: 'Текст-рыба для веб-дизайна: лорем ипсум долор сит амет, consectetur adipiscing элит. Элиас интеллегам мел ид, при пертинах хонистас ут. Не про вирис сапиентем, вис номинави детрахит адиписцинг ут. Яуидам сенсибус вис ид, ин про убикве ирьяредем. Эос ут обликве веритус.',
     available: true
   },
   {
     userId: 7,
     imageId: 'https://once-upon-an-easel-bucket.s3.us-east-1.amazonaws.com/usher-art/palace.jpg',
     title: 'Palace',
-    description: '',
+    description: 'Текст-рыба для веб-дизайна: лорем ипсум долор сит амет, consectetur adipiscing элит. Элиас интеллегам мел ид, при пертинах хонистас ут. Не про вирис сапиентем, вис номинави детрахит адиписцинг ут. Яуидам сенсибус вис ид, ин про убикве ирьяредем. Эос ут обликве веритус.',
     available: true
   },
   {
     userId: 7,
     imageId: 'https://once-upon-an-easel-bucket.s3.us-east-1.amazonaws.com/usher-art/oncloudsofglory.jpg',
     title: 'On Clouds of Glory',
-    description: '',
+    description: 'Текст-рыба для веб-дизайна: лорем ипсум долор сит амет, consectetur adipiscing элит. Элиас интеллегам мел ид, при пертинах хонистас ут. Не про вирис сапиентем, вис номинави детрахит адиписцинг ут. Яуидам сенсибус вис ид, ин про убикве ирьяредем. Эос ут обликве веритус.',
     available: true
   },
   {
     userId: 7,
     imageId: 'https://once-upon-an-easel-bucket.s3.us-east-1.amazonaws.com/usher-art/catchmewhenifall.jpg',
     title: 'Catch Me When I Fall',
-    description: '',
+    description: 'Текст-рыба для веб-дизайна: лорем ипсум долор сит амет, consectetur adipiscing элит. Элиас интеллегам мел ид, при пертинах хонистас ут. Не про вирис сапиентем, вис номинави детрахит адиписцинг ут. Яуидам сенсибус вис ид, ин про убикве ирьяредем. Эос ут обликве веритус.',
     available: true
   },
   {
     userId: 7,
     imageId: 'https://once-upon-an-easel-bucket.s3.us-east-1.amazonaws.com/usher-art/AB_005.jpg',
     title: 'Building Worlds',
-    description: '',
+    description: 'Текст-рыба для веб-дизайна: лорем ипсум долор сит амет, consectetur adipiscing элит. Элиас интеллегам мел ид, при пертинах хонистас ут. Не про вирис сапиентем, вис номинави детрахит адиписцинг ут. Яуидам сенсибус вис ид, ин про убикве ирьяредем. Эос ут обликве веритус.',
     available: true
   },
   {
     userId: 8,
     imageId: 'https://once-upon-an-easel-bucket.s3.us-east-1.amazonaws.com/usher-art/vehiclejpg',
     title: 'Vehicle',
-    description: '',
+    description: 'Текст-рыба для веб-дизайна: лорем ипсум долор сит амет, consectetur adipiscing элит. Элиас интеллегам мел ид, при пертинах хонистас ут. Не про вирис сапиентем, вис номинави детрахит адиписцинг ут. Яуидам сенсибус вис ид, ин про убикве ирьяредем. Эос ут обликве веритус.',
     available: true
   },
   {
     userId: 8,
     imageId: 'https://once-upon-an-easel-bucket.s3.us-east-1.amazonaws.com/usher-art/kapparos.jpg',
     title: 'Kapparos',
-    description: '',
+    description: 'Текст-рыба для веб-дизайна: лорем ипсум долор сит амет, consectetur adipiscing элит. Элиас интеллегам мел ид, при пертинах хонистас ут. Не про вирис сапиентем, вис номинави детрахит адиписцинг ут. Яуидам сенсибус вис ид, ин про убикве ирьяредем. Эос ут обликве веритус.',
     available: true
   },
   {
     userId: 8,
     imageId: 'https://once-upon-an-easel-bucket.s3.us-east-1.amazonaws.com/usher-art/prayinginblue.jpg',
     title: 'Praying in Blue',
-    description: '',
+    description: 'Текст-рыба для веб-дизайна: лорем ипсум долор сит амет, consectetur adipiscing элит. Элиас интеллегам мел ид, при пертинах хонистас ут. Не про вирис сапиентем, вис номинави детрахит адиписцинг ут. Яуидам сенсибус вис ид, ин про убикве ирьяредем. Эос ут обликве веритус.',
     available: true
   },
   // {
