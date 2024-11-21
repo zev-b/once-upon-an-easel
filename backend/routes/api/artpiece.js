89687f0afc0142453ece2ce22af3fcb088f2c0f6
zev-b <zevbistritz@gmail.com>
2024-11-21 16:52:23 -0500

    starting routes

--- backend/routes/api/artpiece.js
+++ backend/routes/api/artpiece.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const { setTokenCookie, requireAuth } = require('../../utils/auth');
-const { User, ArtPiece, Tag } = require('../../db/models'); 
 const { check } = require('express-validator');
 const { handleValidationErrors } = require('../../utils/validation');
 const { Op } = require("sequelize");
+
+const { User, ArtPiece, Tag } = require('../../db/models'); 
 const router = express.Router();
