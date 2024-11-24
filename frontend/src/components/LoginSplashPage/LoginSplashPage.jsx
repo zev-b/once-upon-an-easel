5f8a0def080d44f2b1ac543fe4a1b5ca00b49f4c
zev-b <zevbistritz@gmail.com>
2024-11-24 13:57:01 -0500

    working on login/splash page component. Brain-break time.

--- frontend/src/components/LoginSplashPage/LoginSplashPage.jsx
+++ frontend/src/components/LoginSplashPage/LoginSplashPage.jsx
@@ -9,23 +9,61 @@ export default function LoginSplashPage() {
     const navigate = useNavigate();
     const dispatch = useDispatch();
 
+    //!    Login
     const [credential, setCredential] = useState("");
     const [password, setPassword] = useState("");
     const [errors, setErrors] = useState({});
     const [buttonDisabled, setButtonDisabled] = useState(true);
+    //!    Signup
+    const [email, setEmail] = useState("");
+    const [username, setUsername] = useState("");
+    const [firstName, setFirstName] = useState("");
+    const [lastName, setLastName] = useState("");
+    const [confirmPassword, setConfirmPassword] = useState("");
 
-    const handleSubmit = (e) => {
+    const handleLogin = (e) => {
         e.preventDefault();
         setErrors({});
         return dispatch(sessionActions.login({ credential, password }))
-          .then(closeModal)
           .catch(async (res) => {
             const data = await res.json();
             if (data && data.errors) {
               setErrors(data.errors);
             }
-          });
+        });
+    };
+
+    const handleSignup = (e) => {
+        e.preventDefault();
+        if (password === confirmPassword) {
+          setErrors({});
+          return dispatch(
+            sessionActions.signup({
+              email,
+              username,
+              firstName,
+              lastName,
+              password
+            })
+          )
+            .catch(async (res) => {
+              const data = await res.json();
+              if (data?.errors) {
+                setErrors(data.errors);
+              }
+            });
+        }
+        return setErrors({
+          confirmPassword: "Confirm Password field must be the same as the Password field"
+        });
       };
+    
+      const isDisabled = !email || 
+                         !username || 
+                         !firstName || 
+                         !lastName || 
+                         password.length < 6 || 
+                         username.length < 4;
 
     useEffect(() => {
         setButtonDisabled(credential.length < 4 || password.length < 6)
@@ -33,28 +71,97 @@ export default function LoginSplashPage() {
 
     const handleDemoLogin = () => {
         return dispatch(sessionActions.login({ credential: "demo-user", password: "password" }))
-          .then(closeModal)
           .catch(async (res) => {
             const data = await res.json();
             if (data && data.errors) {
               setErrors(data.errors);
             }
-          });
-      };
+        });
+    };
 
     return (
         <>
-      <h1>Log In</h1>
+        ( <h1>Log In</h1>
+        <form onSubmit={handleLogin}>
+            <label>
+            Username or Email
+            <input
+                type="text"
+                value={credential}
+                onChange={(e) => setCredential(e.target.value)}
+                required
+            />
+            </label>
+            <label>
+            Password
+            <input
+                type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                required
+            />
+            </label>
+            {errors.credential && (
+            <p>{errors.credential}</p>
+            )}
+            <button 
+            type="submit"
+            disabled={buttonDisabled}
+            >
+            Log In
+            </button>
+        </form>
+
+        <button 
+        onClick={handleDemoLogin} 
+        className="demo-login-button" 
+        style={{margin: '0 auto'}}
+        >
+            Log in as Demo User
+        </button>
+        ) : (
+            <h1>Sign Up</h1>
       <form onSubmit={handleSubmit}>
         <label>
-          Username or Email
+          Email
           <input
             type="text"
-            value={credential}
-            onChange={(e) => setCredential(e.target.value)}
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
             required
           />
         </label>
+        {errors.email && <p>{errors.email}</p>}
+        <label>
+          Username
+          <input
+            type="text"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            required
+          />
+        </label>
+        {errors.username && <p>{errors.username}</p>}
+        <label>
+          First Name
+          <input
+            type="text"
+            value={firstName}
+            onChange={(e) => setFirstName(e.target.value)}
+            required
+          />
+        </label>
+        {errors.firstName && <p>{errors.firstName}</p>}
+        <label>
+          Last Name
+          <input
+            type="text"
+            value={lastName}
+            onChange={(e) => setLastName(e.target.value)}
+            required
+          />
+        </label>
+        {errors.lastName && <p>{errors.lastName}</p>}
         <label>
           Password
           <input
@@ -64,24 +171,22 @@ export default function LoginSplashPage() {
             required
           />
         </label>
-        {errors.credential && (
-          <p>{errors.credential}</p>
+        {errors.password && <p>{errors.password}</p>}
+        <label>
+          Confirm Password
+          <input
+            type="password"
+            value={confirmPassword}
+            onChange={(e) => setConfirmPassword(e.target.value)}
+            required
+          />
+        </label>
+        {errors.confirmPassword && (
+          <p>{errors.confirmPassword}</p>
         )}
-        <button 
-        type="submit"
-        disabled={buttonDisabled}
-        >
-          Log In
-        </button>
+        <button type="submit" disabled={isDisabled}>Sign Up</button>
       </form>
-
-      <button 
-      onClick={handleDemoLogin} 
-      className="demo-login-button" 
-      style={{margin: '0 auto'}}
-      >
-        Log in as Demo User
-      </button>
+        )
     </>
   );
 }
