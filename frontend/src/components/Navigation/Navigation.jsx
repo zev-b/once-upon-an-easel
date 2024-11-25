30ce6c53f7ffa1e7c3e8288b8abb2bfd7620d4a1
zev-b <zevbistritz@gmail.com>
2024-11-24 21:44:31 -0500

    Starting to work on Navigation/GalleryHome components creating the part of navigation for usr dropdown of profile button and Modal stuff

---
+++ frontend/src/components/Navigation/Navigation.jsx
@@ -0,0 +1,25 @@
+// frontend/src/components/Navigation/Navigation.jsx
+import { Link, NavLink } from "react-router-dom";
+import { useSelector } from "react-redux";
+import ProfileButton from "./ProfileButton";
+import "./Navigation.css";
+
+export default function Navigation({ isLoaded }) {
+    const sessionUser = useSelector(state => state.session.user);
+
+    return (
+        <ul>
+            <li>
+                <NavLink to="/artpieces" >
+                <img src="" alt="return to gallery home button" />
+                </NavLink>
+            </li>
+            {isLoaded && (
+                <li>
+                    {/* {!!sessionUser && ( Button to open post-art-modal here )} */}
+                    <ProfileButton user={sessionUser} />
+                </li>
+            )}
+        </ul>
+    )
+}
---
+++ frontend/src/components/Navigation/ProfileButton.jsx
@@ -0,0 +1,38 @@
+import { useEffect, useRef } from "react";
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import * as sessionActions from '../../store/session';
+
+export default function ProfileButton({ user }) {
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+
+    const [showMenu, setShowMenu] = useState(false);
+    const ulRef = useRef();
+
+    const toggleMenu = (e) => {
+        e.stopPropagation();
+        setShowMenu(!showMenu);
+    }
+
+    useEffect(() => {
+        if (!showMenu) return;
+        
+        const closeMenu = (e) => {
+            if (!ulRef.current.contains(e.target)) setShowMenu(false);
+        }
+        document.addEventListener('click', closeMenu);
+        return () => document.removeEventListener('click', closeMenu);
+    }, [showMenu])
+
+    const closeMenu = () => setShowMenu(false);
+
+    const logout = (e) => {
+        e.preventDefault();
+        dispatch(sessionActions.logout());
+        closeMenu();
+        navigate("/")
+    };
+
+    const ulClassName = "profile-dropdown" + (showMenu ? "" : " hidden");
+}
