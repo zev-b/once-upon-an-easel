15d9e96033d612cd04c9f119a453ce0d9701f75a
zev-b <zevbistritz@gmail.com>
2024-12-01 00:36:18 -0500

    Added package.json to root for render deploy

--- backend/db/migrations/20241120221135-create-art-piece.js
+++ backend/db/migrations/20241120221135-create-art-piece.js
@@ -36,7 +36,7 @@ module.exports = {
         allowNull: false,
       },
       description: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING(315)
       },
       available: Sequelize.BOOLEAN,
       createdAt: {
